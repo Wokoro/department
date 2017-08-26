cbf926729e0d79d1112fed7fd70ffbb6494e4994
Wokoro D. Samuel <wokorosamuel@yahoo.com>
2017-08-25 21:12:50 -0700

    fix1

--- app/models/assignment_submission.rb
+++ app/models/assignment_submission.rb
@@ -1,5 +1,5 @@
 class AssignmentSubmission < ApplicationRecord
 	belongs_to :student
 	belongs_to :assignment
-	belongs_to :lecturer through: assignments
+	belongs_to :lecturer through: :assignments
 end
